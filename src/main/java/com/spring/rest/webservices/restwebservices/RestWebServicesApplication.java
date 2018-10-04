d2f9c25bbd18bd4da141c439959a1790a8cae8ef
Diouf, Patrice <pdiouf@cebglobal.com>
2018-10-03 22:55:12 -0400

    reset pom file

--- src/main/java/com/spring/rest/webservices/restwebservices/RestWebServicesApplication.java
+++ src/main/java/com/spring/rest/webservices/restwebservices/RestWebServicesApplication.java
@@ -2,13 +2,9 @@ package com.spring.rest.webservices.restwebservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
 
