3d4bb6bde994bf4c3d79f8a0add6ed2e3aaf2fd4
Diouf, Patrice <pdiouf@cebglobal.com>
2018-11-10 23:57:10 -0500

    implement user connexion

--- src/main/java/com/spring/rest/webservices/restwebservices/controller/QueueController.java
+++ src/main/java/com/spring/rest/webservices/restwebservices/controller/QueueController.java
@@ -1,112 +1,112 @@
-package com.spring.rest.webservices.restwebservices.controller;
-
-
-import com.spring.rest.webservices.restwebservices.Exception.ResourceNotNotFoundException;
-import com.spring.rest.webservices.restwebservices.model.Queue;
-import com.spring.rest.webservices.restwebservices.model.User;
-import com.spring.rest.webservices.restwebservices.repository.QueueRepository;
-import com.spring.rest.webservices.restwebservices.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.Resource;
-import org.springframework.hateoas.mvc.ControllerLinkBuilder;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import java.net.URI;
-import java.util.Optional;
-import java.util.Set;
-
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
-
-@RestController
-public class QueueController {
-
-    @Autowired
-    private QueueRepository queueRepository;
-
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @GetMapping("/users/{id}/queues")
-    public Set<Queue> retrieveAllUserQueues(@PathVariable int id ) {
-        Optional<User> userOptional = userRepository.findById(id);
-
-        if (!userOptional.isPresent()) {
-            throw new ResourceNotNotFoundException("id-" + id);
-        }
-        return userOptional.get().getQueues();
-    }
-
-    @PostMapping("/users/{id}/queues")
-    public ResponseEntity<Object> createQueue(@PathVariable int id, @RequestBody Queue queue) {
-        Optional<User> userOptional = userRepository.findById(id);
-        if (!userOptional.isPresent()) {
-            throw new ResourceNotNotFoundException("id-" + id);
-        }
-        User user = userOptional.get();
-
-        queue.setOwner(user);
-        queueRepository.save(queue);
-
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(queue.getId()).toUri();
-        return ResponseEntity.created(location).build();
-    }
-
-    @GetMapping("/users/{userId}/queues/{id}")
-    public Resource<Queue> retrieveQueue(@PathVariable int id, @PathVariable int userId) {
-        Optional<Queue> queue = queueRepository.findById(id);
-        if (!userRepository.existsById(userId)) {
-            throw new ResourceNotNotFoundException("id-" + userId);
-        }
-        if (!queue.isPresent()) {
-            throw new ResourceNotNotFoundException("id-" + id);
-        }
-
-        Resource<Queue> resource = new Resource<>(queue.get());
-        ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUserQueues(userId));
-        resource.add(linkTo.withRel("all-queues"));
-
-        return resource;
-
-    }
-
-    @DeleteMapping("/users/{userId}/queues/{id}")
-    public void deleteQueue(@PathVariable int id) {
-        queueRepository.deleteById(id);
-    }
-
-
-    // member ressource
-
-
-    @GetMapping("/queues/{id}/members")
-    public Set<User> retrieveAllQueueMembers(@PathVariable int id){
-        Optional<Queue> queueOptional = queueRepository.findById(id);
-
-        if((!queueOptional.isPresent())){
-            throw  new ResourceNotNotFoundException("id-" +id);
-        }
-        return queueOptional.get().getMembers();
-    }
-
-    @PostMapping("/queues/{id}/members/{m_id}")
-    public ResponseEntity<Object> AddQueueMember(@PathVariable int id, @PathVariable int m_id){
-        Optional<Queue> queueOptional = queueRepository.findById(id);
-        Optional<User> userOptional = userRepository.findById(m_id);
-        if(!queueOptional.isPresent() || !userOptional.isPresent()){
-            throw  new ResourceNotNotFoundException(("id-" +id +" id-"+m_id));
-        }
-        Queue queue = queueOptional.get();
-        User member = userOptional.get();
-        queue.getMembers().add(member);
-        userRepository.save(member);
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(member.getId()).toUri();
-
-        return ResponseEntity.created(location).build();
-    }
-
-
-}
+//package com.spring.rest.webservices.restwebservices.controller;
+//
+//
+//import com.spring.rest.webservices.restwebservices.Exception.ResourceNotNotFoundException;
+//import com.spring.rest.webservices.restwebservices.model.Queue;
+//import com.spring.rest.webservices.restwebservices.model.User;
+//import com.spring.rest.webservices.restwebservices.repository.QueueRepository;
+//import com.spring.rest.webservices.restwebservices.repository.UserRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.hateoas.Resource;
+//import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+//
+//import java.net.URI;
+//import java.util.Optional;
+//import java.util.Set;
+//
+//import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+//import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+//
+//@RestController
+//public class QueueController {
+//
+//    @Autowired
+//    private QueueRepository queueRepository;
+//
+//
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @GetMapping("/users/{id}/queues")
+//    public Set<Queue> retrieveAllUserQueues(@PathVariable Long id ) {
+//        Optional<User> userOptional = userRepository.findById(id);
+//
+//        if (!userOptional.isPresent()) {
+//            throw new ResourceNotNotFoundException("id-" + id);
+//        }
+//        return userOptional.get().getQueues();
+//    }
+//
+//    @PostMapping("/users/{id}/queues")
+//    public ResponseEntity<Object> createQueue(@PathVariable Long id, @RequestBody Queue queue) {
+//        Optional<User> userOptional = userRepository.findById(id);
+//        if (!userOptional.isPresent()) {
+//            throw new ResourceNotNotFoundException("id-" + id);
+//        }
+//        User user = userOptional.get();
+//
+//        queue.setOwner(user);
+//        queueRepository.save(queue);
+//
+//        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(queue.getId()).toUri();
+//        return ResponseEntity.created(location).build();
+//    }
+//
+//    @GetMapping("/users/{userId}/queues/{id}")
+//    public Resource<Queue> retrieveQueue(@PathVariable int id, @PathVariable Long userId) {
+//        Optional<Queue> queue = queueRepository.findById(id);
+//        if (!userRepository.existsById(userId)) {
+//            throw new ResourceNotNotFoundException("id-" + userId);
+//        }
+//        if (!queue.isPresent()) {
+//            throw new ResourceNotNotFoundException("id-" + id);
+//        }
+//
+//        Resource<Queue> resource = new Resource<>(queue.get());
+//        ControllerLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUserQueues(userId));
+//        resource.add(linkTo.withRel("all-queues"));
+//
+//        return resource;
+//
+//    }
+//
+//    @DeleteMapping("/users/{userId}/queues/{id}")
+//    public void deleteQueue(@PathVariable int id) {
+//        queueRepository.deleteById(id);
+//    }
+//
+//
+//    // member ressource
+//
+//
+//    @GetMapping("/queues/{id}/members")
+//    public Set<User> retrieveAllQueueMembers(@PathVariable int id){
+//        Optional<Queue> queueOptional = queueRepository.findById(id);
+//
+//        if((!queueOptional.isPresent())){
+//            throw  new ResourceNotNotFoundException("id-" +id);
+//        }
+//        return queueOptional.get().getMembers();
+//    }
+//
+//    @PostMapping("/queues/{id}/members/{m_id}")
+//    public ResponseEntity<Object> AddQueueMember(@PathVariable int id, @PathVariable Long m_id){
+//        Optional<Queue> queueOptional = queueRepository.findById(id);
+//        Optional<User> userOptional = userRepository.findById(m_id);
+//        if(!queueOptional.isPresent() || !userOptional.isPresent()){
+//            throw  new ResourceNotNotFoundException(("id-" +id +" id-"+m_id));
+//        }
+//        Queue queue = queueOptional.get();
+//        User member = userOptional.get();
+//        queue.getMembers().add(member);
+//        userRepository.save(member);
+//        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(member.getId()).toUri();
+//
+//        return ResponseEntity.created(location).build();
+//    }
+//
+//
+//}
--- src/main/java/com/spring/rest/webservices/restwebservices/controller/UserController.java
+++ src/main/java/com/spring/rest/webservices/restwebservices/controller/UserController.java
@@ -2,10 +2,10 @@ package com.spring.rest.webservices.restwebservices.controller;
 
 
 import com.spring.rest.webservices.restwebservices.Exception.ResourceNotNotFoundException;
-import com.spring.rest.webservices.restwebservices.model.Queue;
-import com.spring.rest.webservices.restwebservices.repository.QueueRepository;
+//import com.spring.rest.webservices.restwebservices.repository.QueueRepository;
 import com.spring.rest.webservices.restwebservices.model.User;
 import com.spring.rest.webservices.restwebservices.repository.UserRepository;
+import com.spring.rest.webservices.restwebservices.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
 
@@ -19,7 +19,14 @@ import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
 
 @RestController
 public class UserController {
@@ -27,8 +34,68 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+//    @Autowired
+//    private QueueRepository queueRepository;
+
+
     @Autowired
-    private QueueRepository queueRepository;
+    private UserService userService;
+
+    @RequestMapping(value= {"/", "/login"}, method=RequestMethod.GET)
+    public ModelAndView login() {
+        ModelAndView model = new ModelAndView();
+
+        model.setViewName("user/login");
+        return model;
+    }
+
+    @RequestMapping(value= {"/signup"}, method=RequestMethod.GET)
+    public ModelAndView signup() {
+        ModelAndView model = new ModelAndView();
+        User user = new User();
+        model.addObject("user", user);
+        model.setViewName("user/signup");
+
+        return model;
+    }
+
+    @RequestMapping(value= {"/signup"}, method=RequestMethod.POST)
+    public ModelAndView createUser(@Valid User user, BindingResult bindingResult) {
+        ModelAndView model = new ModelAndView();
+        User userExists = userService.findUserByEmail(user.getEmail());
+
+        if(userExists != null) {
+            bindingResult.rejectValue("email", "error.user", "This email already exists!");
+        }
+        if(bindingResult.hasErrors()) {
+            model.setViewName("user/signup");
+        } else {
+            userService.saveUser(user);
+            model.addObject("msg", "User has been registered successfully!");
+            model.addObject("user", new User());
+            model.setViewName("user/signup");
+        }
+
+        return model;
+    }
+
+    @RequestMapping(value= {"/home/home"}, method=RequestMethod.GET)
+    public ModelAndView home() {
+        ModelAndView model = new ModelAndView();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByEmail(auth.getName());
+
+        model.addObject("userName", user.getFirstname() + " " + user.getLastname());
+        model.setViewName("home/home");
+        return model;
+    }
+
+    @RequestMapping(value= {"/access_denied"}, method=RequestMethod.GET)
+    public ModelAndView accessDenied() {
+        ModelAndView model = new ModelAndView();
+        model.setViewName("errorss/access_denied");
+        return model;
+    }
 
     @GetMapping("/users")
     public List<User> retrieveAllUsers() {
@@ -44,7 +111,7 @@ public class UserController {
     }
 
     @GetMapping("/users/{id}")
-    public Resource<User>  retrieveUser(@PathVariable int id) {
+    public Resource<User>  retrieveUser(@PathVariable Long id) {
         Optional<User> user = userRepository.findById(id);
         if (!user.isPresent()) {
             throw new ResourceNotNotFoundException("id-" + id);
@@ -61,7 +128,7 @@ public class UserController {
 
     }
     @DeleteMapping("/users/{id}")
-    public void  deleteUser(@PathVariable int id) {
+    public void  deleteUser(@PathVariable Long id) {
         userRepository.deleteById(id);
     }
 
